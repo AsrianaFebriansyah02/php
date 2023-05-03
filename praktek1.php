57d223e35b112883ac39ef6c84465b21db321206
AsrianaFebriansyah02 <124341045+AsrianaFebriansyah02@users.noreply.github.com>
2023-05-03 19:38:18 +0700

    Add files via upload
    praktek1

---
+++ praktek1.php
@@ -0,0 +1,9 @@
+<?php
+for ($bil = 1; $bil <= 9; $bil++) {
+    if ($bil % 2 == 0) {
+        echo "$bil adalah bilangan genap";
+    } else {
+        echo "$bil adalah bilangan ganjil";
+    }
+    echo "<br>";
+}
